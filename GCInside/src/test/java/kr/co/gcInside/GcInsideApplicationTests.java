f628ed7783a97923fc326159c740650d244b8d80
hochmun <s01099760587@gmail.com>
2023-02-27 18:26:50 +0900

    2023/02/27 // 심규영 // spring project 추가

---
+++ GCInside/src/test/java/kr/co/gcInside/GcInsideApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.gcInside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcInsideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
