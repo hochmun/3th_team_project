1e3170687b59e3f11935523b5010ffdd1fdede0c
aowlrxm15 <aowlrxm15@gmail.com>
2023-03-08 19:32:04 +0900

    2023 // 전인준 // member화면구현완료,약관 기능구현시작

--- GCInside/src/main/java/kr/co/gcInside/controller/MemberContoller.java
+++ GCInside/src/main/java/kr/co/gcInside/controller/MemberContoller.java
@@ -1,4 +1,35 @@
 package kr.co.gcInside.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MemberContoller {
+
+    @GetMapping("member/login")
+    public String login(){
+        return "member/login";
+    }
+    @GetMapping("member/terms")
+    public String terms(){
+        return "member/terms";
+    }
+    @GetMapping("member/register")
+    public String register(){
+        return "member/register";
+    }
+    @GetMapping("member/find_id")
+    public String find_id(){
+        return "member/find_id";
+    }
+    @GetMapping("member/reset_pwd")
+    public String reset_pwd(){
+        return "member/reset_pwd";
+    }
+    @GetMapping("member/reset_pwd_Result")
+    public String reset_pwd_Result(){
+        return "member/reset_pwd_Result";
+    }
+
 }
