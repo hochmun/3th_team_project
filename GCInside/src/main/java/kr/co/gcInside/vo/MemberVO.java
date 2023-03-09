bff2a5b83237b4c538a7264e25f07e5edf4a631a
aowlrxm15 <aowlrxm15@gmail.com>
2023-03-09 19:26:46 +0900

    2023-03-09 // 전인준 // 약관기능구현완료,회원가입기능구현시작

--- GCInside/src/main/java/kr/co/gcInside/vo/MemberVO.java
+++ GCInside/src/main/java/kr/co/gcInside/vo/MemberVO.java
@@ -1,4 +1,25 @@
 package kr.co.gcInside.vo;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class MemberVO {
+    private String member_uid;
+    private String member_pass;
+    private String member_email;
+    private String member_nick;
+    private int member_point;
+    private String member_regip;
+    private int member_grade;
+    private int member_status;
+    private String member_rdate;
+    private String member_wdate;
+    private String member_sanctions_rdate;
+    private String member_sanctions_wdate;
+    private int member_sanctions_grade;
+
 }
