5d3b6b8edb1f763dac558fd8b497d6102b4aeaa7
soungjun <jungy0309@naver.com>
2023-03-10 19:44:22 +0900

    2023/03/10

--- HTML/js/minor/minor.js
+++ HTML/js/minor/minor.js
@@ -5,18 +5,3 @@ function showLayer(layerName){
 		$("#"+layerName).hide();
 	}
 }
-// function thumb_view(e,src) {
-// 	$("#gallery_thumb_img").attr("src",src);
-// 	var ex_obj = document.getElementById('gallery_thumb_div');
-//     if(!e) e = window.Event;
-//     pos = abspos(e);
-//     ex_obj.style.left = pos.x+"px";
-//     ex_obj.style.top = (pos.y+10)+"px";
-//     ex_obj.style.display = ex_obj.style.display=='none'?'block':'none';
-    
-// }
-
-// function thumb_hide(e,src) {
-// 	var ex_obj = document.getElementById('gallery_thumb_div');
-//     ex_obj.style.display = 'none';
-// }
