ac1a7c575cab5ac2ef92211c8724849a0a912a0c
soungjun <jungy0309@naver.com>
2023-03-28 14:24:33 +0900

    123

--- GCInside/src/main/java/kr/co/gcInside/controller/Mindex.java
+++ GCInside/src/main/java/kr/co/gcInside/controller/Mindex.java
@@ -6,15 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 /**
  * 2023.03.27 // 라성준 // 컨트롤러 생성
  */
-//@Controller
-//public class Mindex {
-//
-//    /**
-//     * 2023.03.27 // 라성준 //
-//     * 갤러리 메인화면 맵핑
-//     */
-//    @GetMapping(value = {"/mini/","m/index"})
-//    public String m() {
-//        return "gall/m/index";
-//    }
-//}
+@Controller
+public class Mindex {
+
+    /**
+     * 2023.03.27 // 라성준 //
+     * 갤러리 메인화면 맵핑
+     */
+    @GetMapping(value = {"/mini/","m/index"})
+    public String m() {
+        return "gall/m/index";
+    }
+}
