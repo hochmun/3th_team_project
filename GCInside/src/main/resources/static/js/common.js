d5b557d64155aafd0015ec9b8ec2cb500838d9bc
hochmun <s01099760587@gmail.com>
2023-04-17 19:03:13 +0900

    2023/04/17 // 심규영 // 갤러리 전날 게시글 갯수, 및 댓글 개수 기능 완료

--- GCInside/src/main/resources/static/js/common.js
+++ GCInside/src/main/resources/static/js/common.js
@@ -1,8 +1,8 @@
 /* 갤러리 수 및 글수 노출 */
 var gall_content_fade = function () {
 	
-	$('.gall_exposure>div:first-child').slideUp();
-    $('.gall_exposure>div').each(function(){
+	$('.gall_exposure > div:first-child').slideUp();
+    $('.gall_exposure > div').each(function(){
         if ($(this).is(':first-child')) {
             $(this).slideUp(
                 function() {
@@ -15,7 +15,6 @@ var gall_content_fade = function () {
 }
 
 $(function() {
-
     // 게시글, 댓글 수 롤링
     setInterval("gall_content_fade()", 3000);
 });
