d14483553545efc5384da4e057b48c688c71657b
hochmun <s01099760587@gmail.com>
2023-04-18 19:51:17 +0900

    2023/04/18 // 심규영 // 자잘한 수정

--- GCInside/src/main/resources/static/js/main/realtime.js
+++ GCInside/src/main/resources/static/js/main/realtime.js
@@ -1,8 +1,8 @@
 /** 2023/04/18 // 심규영 // 실시간 베스트 스크립트 */
 $(()=>{
     realtimePgGetArticle(1); // 처음 페이지 로드 될시 1페이지 불러오기
-    $('.realtime_prev').click(()=>{realtimePagePrev();});
-    $('.realtime_next').click(()=>{realtimePageNext();});
+    $('.realtime_prev').click(()=>{realtimePageMove(-1);});
+    $('.realtime_next').click(()=>{realtimePageMove(1);});
 });
 
 // 페이지에 따른 게시글 불러오는 함수
@@ -33,6 +33,25 @@ const realtimeGetArticleListAjax = (jsonData) => {
 };
 
 // 페이지 이동 함수
+const realtimePageMove = (mode) => {
+    const pg = $('.realtimeNum')[0].innerText; // 페이지 현재 번호
+    const nextPg = parseInt(pg)+mode;
+    if(nextPg == '7') nextPg = '1';
+    if(nextPg == '0') nextPg = '6';
+
+    // 페이지 번호 변경
+    $('.realtimeNum').text(nextPg);
+
+    // 페이지 출력 변경
+    $('.time_best .typet_list.p_'+pg).hide();
+    $('.time_best .typet_list.p_'+nextPg).show();
+
+    // 불러온 내용이 없을 경우
+    if($('.time_best .typet_list.p_'+nextPg+' > li').length == 0) {
+        realtimePgGetArticle(nextPg);
+    }
+};
+
 const realtimePageNext = () => {
     const pg = $('.realtimeNum')[0].innerText; // 페이지 현재 번호
     const nextPg = pg == '6' ? '1' : parseInt(pg)+1;
