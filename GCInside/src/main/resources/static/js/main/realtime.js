3730e33644c50a0046a50f68c055b065d1c9b9b1
hochmun <s01099760587@gmail.com>
2023-04-19 19:42:05 +0900

    2023/04/19 // 심규영 // 실시간 베스트 출력은 완료

--- GCInside/src/main/resources/static/js/main/realtime.js
+++ GCInside/src/main/resources/static/js/main/realtime.js
@@ -1,6 +1,6 @@
 /** 2023/04/18 // 심규영 // 실시간 베스트 스크립트 */
 $(()=>{
-    realtimeFunction().PgGetArticle(1); // 처음 페이지 로드 될시 1페이지 불러오기
+    realtimeFunction().pageMove(0); // 처음 페이지 로드 될시 1페이지 불러오기
     $('.realtime_prev').click(()=>{realtimeFunction().pageMove(-1);});
     $('.realtime_next').click(()=>{realtimeFunction().pageMove(1);});
 });
@@ -8,11 +8,40 @@ $(()=>{
 // 중첩 함수 생성
 const realtimeFunction = () => {
 
+    // 해당하는 장소에 데이터 집어 넣는 함수
+    const insertArticles = (articleVOS, pg) => {
+        articleVOS.forEach((articleVO)=>{
+            console.log("articleVO : ",articleVO);
+            $('<li>').append(
+                $('<a>').attr('href','/GCInside/m/board/view/?id=gcbest&no='+articleVO.article_num)
+                .attr('class','main_log').append(
+                    $('<div>').attr('class','box bestimg').append(
+                        articleVO.article_file > 0 ? $('<img>').attr('src',articleVO.file_url).attr('style','position: relative;') : ''
+                    )
+                ).append(
+                    $('<div>').attr('class','box besttxt').append(
+                        $('<p>').text(articleVO.article_title)
+                    ).append(
+                        $('<span>').attr('class','num').text('['+articleVO.article_comment_count+'}')
+                    )
+                ).append(
+                    $('<div>').attr('class','box best_info').append(
+                        $('<span>').attr('class','name').text(articleVO.article_login_status == 0 ? articleVO.article_uid : articleVO.article_nonmember_uid)
+                    ).append(
+                        $('<span>').attr('class','time').text(articleVO.article_rdate)
+                    )
+                )
+            ).appendTo('.time_best .typet_list.p_'+pg);
+        });
+    };
+
     // 페이지에 따른 게시글 불러오는 함수
     const PgGetArticle = async (pg) => {
         // 페이지 번호에 따른 게시글 가져오기
         const articleVOS = await GetArticleListAjax({"pg":pg});
         console.log("articleVOS : ",articleVOS);
+
+        insertArticles(articleVOS, pg);
     };
 
     // 페이지 내용 불러오는 ajax 함수
@@ -25,7 +54,7 @@ const realtimeFunction = () => {
                 data: JSON.stringify(jsonData),
                 dataType:'json',
                 success: function(data) {
-                    return resolve(data);
+                    return resolve(data.articleVOS);
                 },
                 error : function(request,status,error){
                     reject(new Error("code = "+ request.status + " message = " + request.responseText + " error = " + error));
@@ -57,7 +86,6 @@ const realtimeFunction = () => {
     // 외부 출력 함수 리턴
     return {
         "pageMove":pageMove,
-        "PgGetArticle":PgGetArticle,
     };
 };
 
