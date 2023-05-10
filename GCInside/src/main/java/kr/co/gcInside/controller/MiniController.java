16e8a98ce2aede12026daa1f614bf7d41f005c4b
hochmun <s01099760587@gmail.com>
2023-05-10 19:37:56 +0900

    2023/05/10 // 심규영 // 메인 갤러리, 마이너 갤러리, 미니 갤러리 인덱스 로그인 표시

--- GCInside/src/main/java/kr/co/gcInside/controller/MiniController.java
+++ GCInside/src/main/java/kr/co/gcInside/controller/MiniController.java
@@ -1,5 +1,8 @@
 package kr.co.gcInside.controller;
 
+import kr.co.gcInside.security.MyUserDetails;
+import kr.co.gcInside.utill.SecurityCheckUtil;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +19,11 @@ public class MiniController {
      * 2023/05/04 // 심규영 // 헤더 이름 구분 추가 // sName
      */
     @GetMapping(value = {"/mini/","mini/index"})
-    public String mini(Model model) {
+    public String mini(Model model, @AuthenticationPrincipal MyUserDetails myUserDetails) {
         model.addAttribute("sName","미니 갤러리");
+        model.addAttribute("authorize", new SecurityCheckUtil().getSecurityInfoDTO(myUserDetails));     // "authorize"라는 이름으로 MyUserDetails 객체를 이용하여 보안 정보 데이터 전달
+
+        if(myUserDetails != null) model.addAttribute("user", myUserDetails.getUser());
 
         return "gall/mini/index";
     }
